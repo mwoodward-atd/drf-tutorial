0663d3ece061b78ceadd6e3784c5895ddb5aa1e3
Matthew Woodward <matthew_woodward@saa.senate.gov>
2020-01-28 05:54:41 -0800

    added login to browsable api

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from snippets.models import Snippet
 from snippets.serializers import UserSerializer, SnippetSerializer
@@ -10,6 +10,7 @@ class SnippetList(generics.ListCreateAPIView):
     """List all code snippets, or create a new snippet"""
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -19,6 +20,7 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """Retrieve, update or delete a code snippet"""
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, ]
 
 
 class UserList(generics.ListAPIView):
